c8b78194c7c3ed35a03f0e0dcada5921ea6e9f8a
Zardinality <asxaqz2@gmail.com>
2019-01-22 19:45:47 +0800

    add random clicking model and abstract class

--- codebase.py
+++ codebase.py
@@ -266,6 +266,7 @@ class YandexData():
 
 # %%
 import random as rd
+<<<<<<< HEAD
 
 class ClickModel(object):
 
@@ -284,8 +285,31 @@ class ClickModel(object):
         return list(map(click_fn, probs))
 
 # %%
+=======
+>>>>>>> add random clicking model and abstract class
+
+class ClickModel(object):
+
+<<<<<<< HEAD
+=======
+    def __init__(self):
+        pass
+    def train(self, data):
+        raise NotImplementedError
+    def get_probs(self, rankings):
+        raise NotImplementedError
+    def is_click(self, rankings, epsilon):
+        """
+        simulate the click, return a boolean list of the same length as `rankings`, True means clicked
+        """
+        probs = self.get_probs(rankings, epsilon)
+        click_fn = lambda p: rd.uniform(0, 1) < p
+        return list(map(click_fn, probs))
+
+# %%
 
 
+>>>>>>> add random clicking model and abstract class
 class RCM(ClickModel):
     """
         Random clicking model
